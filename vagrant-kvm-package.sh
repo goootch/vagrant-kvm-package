afd997ef5510b39b3c67de6a6d990a323190b8ca
Hiroki Goto <h.gotou.0120@gmail.com>
2014-08-13 18:40:00 +0900

    Support backing images.
    Box化対象のイメージファイルがベースイメージを参照している場合は、非参照となるようイメージファイルを変換する。

--- vagrant-kvm-package.sh
+++ vagrant-kvm-package.sh
@@ -7,10 +7,33 @@ error() {
 }
 
 usage() {
-    echo "Usage: ${0} NAME IMAGE"
+  echo "Usage: ${0} NAME IMAGE"
     echo
     echo "Package a kvm qcow2 image into a kvm vagrant reusable box"
     echo "It uses virt-install to do so"
+}
+
+is_backing_file() {
+  local img=$(readlink -e $1)
+  qemu-img info $img | egrep -q "^backing file:"
+  if [ "$?" -eq 0 ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+convert_to_nonbacking_file() {
+  local in_img=$(readlink -e $1)
+  local out_img=$2
+	echo "Converting to a non-backing file. ($out_img)"
+  qemu-img convert -c -p -O qcow2 $in_img $out_img
+  if [ "$?" -ne 0 ]; then
+    rm -rf $out_img
+    return 1
+  else
+    return 0
+  fi
 }
 
 if [ -z "$2" ]; then
@@ -20,8 +43,8 @@ fi
 
 # defaults for virtual server
 NAME="$1"
-RAM=2048
-VCPUS=2
+RAM=512
+VCPUS=1
 
 IMG=$(readlink -e $2)
 IMG_BASENAME=$(basename $IMG)
@@ -34,14 +57,19 @@ mkdir -p $TMP_DIR
 # We move the image to the tempdir
 # ensure that it's moved back again
 # and the tmp dir removed
-trap "mv $TMP_DIR/$IMG_BASENAME $IMG_DIR; rm -rf $TMP_DIR" EXIT
+trap "(test -f $TMP_DIR/$IMG_BASENAME && mv $TMP_DIR/$IMG_BASENAME $IMG_DIR); rm -rf $TMP_DIR" EXIT
 
-mv $IMG $TMP_DIR
-IMG=$TMP_DIR/$IMG_BASENAME
-
-cd $TMP_DIR
+# convert to non-backing file, if the image-file is "backing file"
+if is_backing_file $IMG ; then
+  convert_to_nonbacking_file $IMG $TMP_DIR/box-disk1.img || exit 1
+  IMG=$TMP_DIR/box-disk1.img
+else
+  mv $IMG $TMP_DIR
+  IMG=$TMP_DIR/$IMG_BASENAME
+fi
 
 # generate box.xml
+cd $TMP_DIR
 
 virt-install \
     --print-xml \
@@ -57,7 +85,7 @@ MAC=$(cat box.xml | grep 'mac address' | cut -d\' -f2 | tr -d :)
 IMG_ABS_PATH=$(cat box.xml | grep 'source file' | cut -d\' -f2)
 
 # replace the absolute image path
-sed -i "s#$IMG_ABS_PATH#$IMG_BASENAME#" box.xml
+sed -i "s#$IMG_ABS_PATH#${IMG##*/}#" box.xml
 
 # Hmm. When not starting the vm (--print-xml) the memory attribute in
 # the XML is missing the unit, which causes an exception in vagrant-kvm
@@ -77,7 +105,8 @@ Vagrant.configure("2") do |config|
 end
 EOF
 
-tar cvzf $NAME.box --totals ./metadata.json ./Vagrantfile ./box.xml ./$IMG_BASENAME
+echo "Creating the a box file. ($IMG_DIR/$NAME.box)"
+tar czf $NAME.box --totals ./metadata.json ./Vagrantfile ./box.xml ./${IMG##*/}
 mv $NAME.box $IMG_DIR
 
-echo "$IMG_DIR/$NAME.box created"
+echo "Complete!"
